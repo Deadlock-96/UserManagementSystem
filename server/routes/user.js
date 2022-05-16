f36c243251d7bc0456361cc7bb2354f9858dac54
deadlock96 <akshaykumar@nimapinfotech.com>
2022-05-16 14:50:41 +0530

    add routes to Add, Edit , Update, Delete and ViewAll

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,12 @@ const userController = require('../controllers/userController');
 //create, find, update, delete
 router.get('/', userController.view);
 router.post('/', userController.find);
-router.get('/addUser', userController.addUser);
+router.get('/addUser', userController.form);
+router.post('/addUser', userController.createUser);
+router.get('/editUser/:id', userController.editUser);
+router.post('/editUser/:id', userController.updateUser);
+router.get('/:id', userController.deleteUser);
+router.get('/viewAllUser/:id', userController.viewAllUsers);
 
 //Router
 // router.get('/', (req, res) => {
